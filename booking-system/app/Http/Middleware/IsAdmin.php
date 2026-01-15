316a6a617cd2f394811b4aabb7d73c49488e55c7
MadlenIlianova <116484349+MadlenIlianova@users.noreply.github.com>
2026-01-15 15:48:26 +0000

    Changing format

--- booking-system/app/Http/Middleware/IsAdmin.php
+++ booking-system/app/Http/Middleware/IsAdmin.php
@@ -8,14 +8,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IsAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-
         if (!auth()->check() || !auth()->user()->is_admin) {
             abort(403, 'Нямате достъп до тази страница. Само администратори имат достъп.');
         }
